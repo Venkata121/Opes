31b32a0897994ee4a55a4b74b8dcd163f3620582
Glitch (sarthaktexas-login) <none>
2020-06-10 07:40:42 +0000

    🚿🎾 Checkpoint ./public/scripts/konami.js:1164060/1991 ./views/layout.pug:1164060/25 ./views/user.pug:1164060/334 ./views/index.pug:1164060/131 ./views/failure.pug:1164060/69 ./views/error.pug:1164060/69 ./routes/users.js:1164060/787 ./app.js:1164060/6997 ./routes/index.js:1164060/427 ./routes/auth.js:1164060/3244 ./views/debug.pug:1164060/681 ./public/stylesheets/style.css:1164060/33 ./routes/debug.js:1164060/432 ./views/toc.pug:1164060/122

--- routes/debug.js
+++ routes/debug.js
@@ -3,11 +3,11 @@ var secured = require("../lib/middleware/secured");
 var router = express.Router();
 
 /* GET user profile. */
-router.get("/user", secured(), function(req, res, next) {
+router.get("/debug", secured(), function(req, res, next) {
   const { _raw, _json, ...userProfile } = req.user;
-  res.render("user", {
+  res.render("debug", {
     userProfile: JSON.stringify(userProfile, null, 2),
-    title: "Profile Page"
+    title: "Debug"
   });
 });
 
