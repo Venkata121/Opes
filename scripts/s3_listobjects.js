c3a959811da95bd3dd66d3a6b9f5672b603f6682
Glitch (sarthaktexas-login) <none>
2020-06-10 15:42:57 +0000

    🌆🦃 Checkpoint ./package.json:1164060/9 ./.aws/credentials:1164060/441 ./scripts/s3_listobjects.js:1164060/57

--- scripts/s3_listobjects.js
+++ scripts/s3_listobjects.js
@@ -1,14 +1,14 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set the region 
-AWS.config.update({region: 'REGION'});
+AWS.config.update({region: 'US-East-Ohio'});
 
 // Create S3 service object
 var s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 // Create the parameters for calling listObjects
 var bucketParams = {
-  Bucket : 'BUCKET_NAME',
+  Bucket : 'cdnsarthak',
 };
 
 // Call S3 to obtain a list of the objects in the bucket
