1549a49cd82fbe13f4ece1d79a32dabcc3acfe12
Glitch (sarthaktexas-login) <none>
2020-06-10 17:37:13 +0000

    🏎🙏 Checkpoint ./routes/auth.js:1164060/34

--- routes/auth.js
+++ routes/auth.js
@@ -43,7 +43,7 @@ router.get("/callback", function(req, res, next) {
 router.get("/logout", (req, res) => {
   req.logout();
 
-  var returnTo = req.protocol + "://" + "rb.sarthakmohanty.me";
+  var returnTo = req.protocol + "://" + req.hostname;
   var port = req.connection.localPort;
   if (port !== undefined && port !== 80 && port !== 443 && port !== 3000) {
     returnTo += ":" + port;
