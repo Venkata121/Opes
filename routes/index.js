f72d82417bfb8c778b3be5b0b0de7f897de3e3e3
Glitch (sarthaktexas-login) <none>
2020-06-11 10:21:47 +0000

    👆🏁 Checkpoint ./routes/index.js:1164060/710

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+
+module.exports = router;
+
 var express = require("express");
+var secured = require("../lib/middleware/secured");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
+/* GET debug page. */
+router.get("/index", secured(), function(req, res, next) {
+  const { _raw, _json, usermetadata, ...userProfile} = req.user;
   res.render("index", {
+    userProfile: JSON.stringify(userProfile, null, 2),
     title: "Sarthak Mohanty"
   });
 });
