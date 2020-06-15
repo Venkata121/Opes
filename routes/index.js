f43f62cbe14167976485eae693b5ff01305e6fd5
Glitch (sarthaktexas-login) <none>
2020-06-15 23:08:31 +0000

    🕹🦀 Checkpoint ./views/resourcessecured.pug:1164060/702 ./routes/index.js:1164060/13470 ./views/resources.pug:1164060/226

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require("express");
 var secured = require("../lib/middleware/secured");
 var router = express.Router();
 var AWS = require("aws-sdk");
-var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+var s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+const pug = require("pug");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -23,10 +24,77 @@ router.get("/debug", secured(), function(req, res, next) {
 /* GET resources page. */
 router.get("/resources", secured(), function(req, res, next) {
   const { _raw, _json, ...userProfile } = req.user;
+  var AWS = require("aws-sdk");
+  var s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+  var subject = [
+    "ArtHistory",
+    "Biology",
+    "CalcBC",
+    "Chemistry",
+    "CompGov",
+    "CompSci",
+    "CompSciA",
+    "EnglishLang",
+    "EnglishLit",
+    "EnvSci",
+    "Euro",
+    "HumanGeo",
+    "Macro",
+    "Micro",
+    "MusicTheory",
+    "Physics1",
+    "Physics2",
+    "PhysicsCEM",
+    "PhysicsCMech",
+    "Psychology",
+    "Statistics",
+    "USGov",
+    "USHistory",
+    "WorldHistory"
+  ];
+  var params = {
+    Bucket: "sarthakcdn",
+    Key: "/secured/QB/" + subject,
+    Expires: 60
+  };
+  var url = s3.getSignedUrl("getObject", params);
+  var compiledSubjectLink = pug.compileFile("resources.pug");
+  console.log(
+    compiledSubjectLink({
+      url: url
+    })
+  );
+  // "<p>Timothy's Pug source code!</p>"
+
   res.render("resources", {
     userProfile: JSON.stringify(userProfile, null, 2),
     title: "Resources",
-    subjects: ["ArtHistory","Biology","CalcBC","Chemistry","CompGov","CompSci","CompSciA","EnglishLang","EnglishLit","EnvSci","Euro","HumanGeo","Macro","Micro","MusicTheory","Physics1","Physics2","PhysicsCEM","PhysicsCMech","Psychology","Statistics","USGov","USHistory","WorldHistory"]
+    subjects: [
+      "ArtHistory",
+      "Biology",
+      "CalcBC",
+      "Chemistry",
+      "CompGov",
+      "CompSci",
+      "CompSciA",
+      "EnglishLang",
+      "EnglishLit",
+      "EnvSci",
+      "Euro",
+      "HumanGeo",
+      "Macro",
+      "Micro",
+      "MusicTheory",
+      "Physics1",
+      "Physics2",
+      "PhysicsCEM",
+      "PhysicsCMech",
+      "Psychology",
+      "Statistics",
+      "USGov",
+      "USHistory",
+      "WorldHistory"
+    ]
   });
 });
 
