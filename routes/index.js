5dd5b498cc9517e7fa383cca06ccda593ade2c49
Glitch (sarthaktexas-login) <none>
2020-06-16 19:34:20 +0000

    🍫🌺 Checkpoint ./routes/index.js:1164060/231

--- routes/index.js
+++ routes/index.js
@@ -78,19 +78,19 @@ ODVirisnt8NMcWirxTWVd/uMfbOiB3Xt3g3LEI4L9bUtUJ76t7lR
   });
 
   res.cookie("CloudFront-Key-Pair-Id", cookie["CloudFront-Key-Pair-Id"], {
-    domain: ".sarthakmohanty.me",
+    domain: ".cloudfront.net",
     path: "/",
     secure: true
   });
 
   res.cookie("CloudFront-Policy", cookie["CloudFront-Policy"], {
-    domain: ".sarthakmohanty.me",
+    domain: ".cloudfront.net",
     path: "/",
     secure: true
   });
 
   res.cookie("CloudFront-Signature", cookie["CloudFront-Signature"], {
-    domain: ".sarthakmohanty.me",
+    domain: ".cloudfront.net",
     path: "/",
     secure: true
   });
