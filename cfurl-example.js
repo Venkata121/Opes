8b9c0f2f1a6a5cfd60a977d6eef5e56df416af42
Glitch (sarthaktexas-resourcebank) <none>
2020-06-22 05:50:54 +0000

    📹🚵 Checkpoint ./s3url-example.js:1164060/15419 ./cfurl-example.js:1164060/21633

--- cfurl-example.js
+++ cfurl-example.js
@@ -1,126 +1,125 @@
+// DO NOT COPY THIS LINE
+let privateKey;
+//
+
+
+// QUESTION BANK FEATURE
+
 var AWS = require("aws-sdk");
-var s3 = new AWS.S3({
-  apiVersion: "2006-03-01",
-  signatureVersion: "v4",
-  region: "us-east-2"
+const cloudFront = new AWS.CloudFront.Signer(
+  process.env.CLOUDFRONT_KEY_PAIR_ID,
+  privateKey
+);
+
+const policy = JSON.stringify({
+  Statement: [
+    {
+      Resource: "http*://d2d3mdelw3jx6o.cloudfront.net/*", // http* => http and https
+      Condition: {
+        DateLessThan: {
+          "AWS:EpochTime": Math.floor(new Date().getTime() / 1000) + 60 * 60 * 1 // Current Time in UTC + time in seconds, (60 * 60 * 1 = 1 hour)
+        }
+      }
+    }
+  ]
 });
-var qbahurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/ArtHistory/index.html",
-  Expires: 300
+
+var qbahurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/ArtHistory/index.html",
+  policy: policy
 });
-var qbburl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/Biology/index.html",
-  Expires: 300
+var qbburl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/Biology/index.html",
+  policy: policy
 });
-var qbcbcurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/CalcBC/index.html",
-  Expires: 300
+var qbcbcurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/CalcBC/index.html",
+  policy: policy
 });
-var qbchemurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/Chemistry/index.html",
-  Expires: 300
+var qbchemurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/Chemistry/index.html",
+  policy: policy
 });
-var qbcgovurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/CompGov/index.html",
-  Expires: 300
+var qbcgovurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/CompGov/index.html",
+  policy: policy
 });
-var qbcsurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/CompSci/index.html",
-  Expires: 300
+var qbcsurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/CompSci/index.html",
+  policy: policy
 });
-var qbcsaurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/CompSciA/index.html",
-  Expires: 300
+var qbcsaurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/CompSciA/index.html",
+  policy: policy
 });
-var qbelaurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/EnglishLang/index.html",
-  Expires: 300
+var qbelaurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/EnglishLang/index.html",
+  policy: policy
 });
-var qbeliurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/EnglishLit/index.html",
-  Expires: 300
+var qbeliurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/EnglishLit/index.html",
+  policy: policy
 });
-var qbesurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/EnvSci/index.html",
-  Expires: 300
+var qbesurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/EnvSci/index.html",
+  policy: policy
 });
-var qbeuurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/Euro/index.html",
-  Expires: 300
+var qbeuurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/Euro/index.html",
+  policy: policy
 });
-var qbhgurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/HumanGeo/index.html",
-  Expires: 300
+var qbhgurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/HumanGeo/index.html",
+  policy: policy
 });
-var qbmaurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/Macro/index.html",
-  Expires: 300
+var qbmaurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/Macro/index.html",
+  policy: policy
 });
-var qbmiurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/Micro/index.html",
-  Expires: 300
+var qbmiurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/Micro/index.html",
+  policy: policy
 });
-var qbmturl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/MusicTheory/index.html",
-  Expires: 300
+var qbmturl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/MusicTheory/index.html",
+  policy: policy
 });
-var qbp1url = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/Physics1/index.html",
-  Expires: 300
+var qbp1url = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/Physics1/index.html",
+  policy: policy
 });
-var qbp2url = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/Physics2/index.html",
-  Expires: 300
+var qbp2url = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/Physics2/index.html",
+  policy: policy
 });
-var qbpcemurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/PhysicsCEM/index.html",
-  Expires: 300
+var qbpcemurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/PhysicsCEM/index.html",
+  policy: policy
 });
-var qbpcmurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/PhysicsCMech/index.html",
-  Expires: 300
+var qbpcmurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/PhysicsCMech/index.html",
+  policy: policy
 });
-var qbpsurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/Psychology/index.html",
-  Expires: 300
+var qbpsurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/Psychology/index.html",
+  policy: policy
 });
-var qbsturl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/Statistics/index.html",
-  Expires: 300
+var qbsturl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/Statistics/index.html",
+  policy: policy
 });
-var qbusgurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/USGov/index.html",
-  Expires: 300
+var qbusgurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/USGov/index.html",
+  policy: policy
 });
-var qbushurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/USHistory/index.html",
-  Expires: 300
+var qbushurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/USHistory/index.html",
+  policy: policy
 });
-var qbwhurl = s3.getSignedUrl("getObject", {
-  Bucket: "sarthakcdn",
-  Key: "secured/QB/WorldHistory/index.html",
-  Expires: 300
+var qbwhurl = cloudFront.getSignedUrl({
+  url: "https://d2d3mdelw3jx6o.cloudfront.net/secured/QB/WorldHistory/index.html",
+  policy: policy
 });
+
+// PERSONAL PROGRESS CHECK FEATURE
+
