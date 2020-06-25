76c1225bb6b715b22ce2ec9f64991b97392c527a
Glitch (sarthaktexas-resourcebank) <none>
2020-06-25 07:44:24 +0000

    Exporting to sarthaktexas/resourcebank ./routes/auth.js:1164060/336

--- routes/authanother.js
+++
@@ -1,8 +0,0 @@
-var admin = require("firebase-admin");
-
-admin.initializeApp(
-  credential: admin.credential.applicationDefault(),
-  databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
-);
-
-export default admin
