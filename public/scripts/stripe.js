8d58d86e68e9cdf8542d4592403108531fdb405f
Glitch (resourcebank) <none>
2020-07-02 20:18:02 +0000

    ⚡️🏉 Checkpoint ./public/scripts/stripe.js:1164060/1211 ./views/payment.pug:1164060/1273

--- public/scripts/stripe.js
+++ public/scripts/stripe.js
@@ -18,3 +18,28 @@ function createCustomer() {
       return result;
     });
 }
+
+// Set your publishable key: remember to change this to your live publishable key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = Stripe('pk_test_JMhCM9X2PO2KydkUw8Cvz8wQ');
+var elements = stripe.elements();
+
+// Set up Stripe.js and Elements to use in checkout form
+var style = {
+  base: {
+    color: "#32325d",
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: "antialiased",
+    fontSize: "16px",
+    "::placeholder": {
+      color: "#aab7c4"
+    }
+  },
+  invalid: {
+    color: "#fa755a",
+    iconColor: "#fa755a"
+  }
+};
+
+var cardElement = elements.create("card", { style: style });
+cardElement.mount("#card-element");
