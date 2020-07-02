a05c981365fb531fbe74845b00ffece61b8cab4f
Glitch (resourcebank) <none>
2020-07-02 00:28:18 +0000

    👯🏕 Checkpoint ./public/scripts/search.js:1164060/3271 ./public/free-resources_data.json:1164060/232 ./views/resourcesnormal.pug:1164060/146

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,43 +1,44 @@
-const resourcesList = document.getElementById('resourcesList');
-const searchBar = document.getElementById('searchBar');
+const resourcesList = document.getElementById("resourcesList");
+const searchBar = document.getElementById("searchBar");
 let freeResources = [];
 
-searchBar.addEventListener('keyup', (e) => {
-    const searchString = e.target.value.toLowerCase();
+searchBar.addEventListener("keyup", e => {
+  const searchString = e.target.value.toLowerCase();
 
-    const filteredResources = freeResources.filter((resource) => {
-        return (
-            resource.subject.toLowerCase().includes(searchString) ||
-            resource.name.toLowerCase().includes(searchString) ||
-            resource.link.toLowerCase().includes(searchString)
-        );
-    });
-    displayResources(filteredResources);
+  const filteredResources = freeResources.filter(resource => {
+    return (
+      resource.subject.toLowerCase().includes(searchString) ||
+      resource.name.toLowerCase().includes(searchString) ||
+      resource.link.toLowerCase().includes(searchString)
+    );
+  });
+  displayResources(filteredResources);
 });
 
 const loadResources = async () => {
-    try {
-        const res = await fetch('/free-resources_data.json');
-        freeResources = await res.json();
-        displayResources(freeResources);
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const res = await fetch("/free-resources_data.json");
+    freeResources = await res.json();
+    displayResources(freeResources);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-const displayResources = (resources) => {
-    const htmlString = resources
-        .map((resource) => {
-            return `
+const displayResources = resources => {
+  const htmlString = resources
+    .map(resource => {
+      return `
             <li class="card card-body customfloatleft">
+                <i class="${resource.icon} card-title"></i>
                 <h6 class="text-warning">${resource.subject}</h5>
                 <h5 class="card-title">${resource.name}</h5>
                 <a href="${resource.link}" class="btn btn-primary">Link</a>
             </li>
         `;
-        })
-        .join('');
-    resourcesList.innerHTML = htmlString;
+    })
+    .join("");
+  resourcesList.innerHTML = htmlString;
 };
 
 loadResources();
